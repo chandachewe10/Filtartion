cc667db83fdb306607b4dd203afa8ea7616718e8
chandachewe10 <chewec03@gmail.com>
2023-02-13 15:23:09 +0000

    Added Skeleton

---
+++ src/Inputs.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Chandachewe\Filtration;
+
+
+
+class Inputs
+{
+    /**
+     * Input String.
+     *
+     * @var string
+     */
+    public $string;
+
+    /**
+     * Input Email.
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     * Input Date.
+     *
+     * @var string
+     */
+    public $date;
+
+    /**
+     * Input URL.
+     *
+     * @var string
+     */
+    public $url;
+
+    /**
+     * Input Password.
+     *
+     * @var string
+     */
+    public $password;
+
+    /**
+     * Input File.
+     *
+     * @var string
+     */
+    public $file;
+
+    /**
+     * Input Image.
+     *
+     * @var string
+     */
+    public $image;
+
+    /**
+     * Input IPV4.
+     *
+     * @var string
+     */
+    public $ipv4;
+
+    /**
+     * Input IPV6.
+     *
+     * @var string
+     */
+    public $ipv6;
+
+    /**
+     * Input Integer.
+     *
+     * @var int
+     */
+    public $integer;
+
+    /**
+     * Input NRC.
+     *
+     * @var string
+     */
+    public $nrc_zm;
+
+    /**
+     * Input Phone.
+     *
+     * @var double
+     */
+    public $phone_zm;
+}
